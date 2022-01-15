06321af387bdde266d0041f500032e9ea10403b2
Tiago Dias <tigo@webdeleve.net>
2022-01-15 05:33:13 -0300

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# :rocket: CRUD com NodeJS, Express, TypeORM e PostgreSQL
+
+[Link - Code/drops 113 da Rocketseat](https://www.youtube.com/watch?v=9AO2hZJsHrs)
---
+++ src/services/DeleteCategoryService.ts
@@ -0,0 +1,19 @@
+import { getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+
+export class DeleteCategoryService {
+
+  async execute(id: string) {
+
+    const repo = getRepository(Category);
+
+    if (!await repo.findOne(id)) {
+      return new Error("Categoriy does not exists");
+    }
+
+    await repo.delete(id);
+
+
+  }
+
+}
